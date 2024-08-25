c6f43c29a91024b0a5aef73bfb4c96dc617ab668
Mostafa Mohmead <mostafamohm200ead@gmail.com>
2024-08-25 22:53:59 +0300

    Add project files.

--- Program.cs
+++ Program.cs
@@ -30,12 +30,24 @@ namespace ContactsProject
 
         static void TestAddNew()
         {
-            clsContact contact = new clsContact();
+            clsContact contact1 = new clsContact();
+            contact1.FirstName = "Mostafa";
+            contact1.LastName = "Mohmead";
+            contact1.Email = "Mo@gmail.com";
+            contact1.Phone = "07888";
+            contact1.Address = "mo-aa";
+            contact1.DateOfBirth = DateTime.Now;
+            contact1.CountryID = 2;
+            contact1.ImagePath = "";
 
+            if(contact1.Save())
+            {
+                Console.WriteLine("Success");
+            }
         }
         static void Main(string[] args)
         {
-            TestFindContact(5);
+            TestAddNew();
         }
     }
 }
