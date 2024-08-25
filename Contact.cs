5609b2fa6d81eaebff6e6840665e7ff499f25d9f
mu17m <122389002+mu17m@users.noreply.github.com>
2024-08-25 21:17:16 +0300

    ContactBusinessLayer

---
+++ Contact.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.SymbolStore;
+using ContactsDataAccessLayer;
+
+namespace ContactsBusinessLayer
+{
+    public class clsContact
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int CountryID { get; set; }
+        public string ImagePath { get; set; }
+
+        enum enMode {AddNew = 1, Update = 2};
+        enMode Mode;
+        public clsContact()
+        {
+            this.ID = -1;
+            this.FirstName = "";
+            this.LastName = "";
+            this.Email = "";
+            this.Phone = "";
+            this.Address = "";
+            this.DateOfBirth = DateTime.Now;
+            this.CountryID = -1;
+            this.ImagePath = "";
+        }
+        private clsContact(int Id, string FirstName, string LastName, string Email, string Phone, string Address, DateTime DateOfBirth, int CountryID, string ImagePath)
+        {
+            this.ID = Id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Phone = Phone;
+            this.Address = Address;
+            this.DateOfBirth = DateOfBirth;
+            this.CountryID = CountryID;
+            this.ImagePath = ImagePath;
+            Mode = enMode.Update;
+        }
+        
+        public bool Save()
+        {
+
+        }
+        public static clsContact Find(int id)
+            {
+            string FirstName = "", LastName = "", Email = "", Phone = "", Address = "", ImagePath = "";
+            DateTime DateOfBirth = DateTime.Now;
+            int CountryID = -1;
+            if (clsContactDataAccess.GetContactInfoByID(id,ref FirstName,ref LastName,ref Email,ref Phone,ref Address,ref DateOfBirth,ref CountryID,ref ImagePath))
+            {
+                return new clsContact(id, FirstName, LastName, Email, Phone, Address,DateOfBirth,CountryID,ImagePath); 
+            }
+            else
+            {
+              return null;
+            }
+            }
+
+
+
+    }
+}
